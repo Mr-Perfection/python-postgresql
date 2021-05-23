46ae0c496711ae6e55af7d8463851622b674a88e
Stephen Lee <sungsoolee0127@gmail.com>
2021-05-22 23:11:13 -0700

    class and static methods

--- practice/class_and_static_methods.py
+++ practice/class_and_static_methods.py
@@ -1,14 +1,18 @@
 class Book:
     TYPES = ("hard", "soft")
-    def __init__(self, name, book_type, weight):
+    def __init__(self, name: str, book_type: str, weight: int):
         self.name = name
         self.book_type = book_type
         self.weight = weight
     
+    def __str__(self):
+        return f'{self.name} {self.book_type} {self.weight}'
     @classmethod
-    def hardcover(cls, name, weight):
-        return cls(name, TYPES[0], weight+100)
+    def hardcover(cls, name, weight) -> "Book":
+        return cls(name, cls.TYPES[0], weight+100)
 
     @staticmethod
-    def helper_method(name, weight):
+    def helper_method(name, weight) -> str:
         return f'{name} {weight}'
+
+print(Book.hardcover('yeezzyy', 100))
